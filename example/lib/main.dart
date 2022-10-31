51182c281dc5641be9a618fccd623d6da82ea84c
sharathrajMindstack <sharathraj@mindstack.in>
2022-10-31 19:46:10 +0530

    example changes done

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const TestPage(),
+      home: const TestPage2(),
     );
   }
 }
@@ -221,6 +221,107 @@ class _TestPageState extends State<TestPage> {
                 const SizedBox(
                   height: 50,
                 )
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          setState(() {});
+        },
+        label: const Text("Submit"),
+      ),
+    );
+  }
+}
+
+class TestPage2 extends StatefulWidget {
+  const TestPage2({Key? key}) : super(key: key);
+
+  @override
+  State<TestPage2> createState() => _TestPage2State();
+}
+
+class _TestPage2State extends State<TestPage2> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  FocusNode searchFocusNode = FocusNode();
+  FocusNode textFieldFocusNode = FocusNode();
+  late SingleValueDropDownController _cnt;
+  late MultiValueDropDownController _cntMulti;
+
+  @override
+  void initState() {
+    _cnt = SingleValueDropDownController();
+    _cntMulti = MultiValueDropDownController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _cnt.dispose();
+    _cntMulti.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                const Text(
+                  "Single selection dropdown with search option",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                DropDownTextField(
+                  // initialValue: "name4",
+                  controller: _cnt,
+                  clearOption: true,
+                  enableSearch: true,
+                  clearIconProperty: IconProperty(color: Colors.green),
+                  searchDecoration: const InputDecoration(
+                      hintText: "enter your custom hint text here"),
+                  validator: (value) {
+                    if (value == null) {
+                      return "Required field";
+                    } else {
+                      return null;
+                    }
+                  },
+                  dropDownItemCount: 6,
+
+                  dropDownList: const [
+                    DropDownValueModel(name: 'name1', value: "value1"),
+                    DropDownValueModel(
+                        name: 'name2',
+                        value: "value2",
+                        toolTipMsg:
+                            "DropDownButton is a widget that we can use to select one unique value from a set of values"),
+                    DropDownValueModel(name: 'name3', value: "value3"),
+                    DropDownValueModel(
+                        name: 'name4',
+                        value: "value4",
+                        toolTipMsg:
+                            "DropDownButton is a widget that we can use to select one unique value from a set of values"),
+                    DropDownValueModel(name: 'name5', value: "value5"),
+                    DropDownValueModel(name: 'name6', value: "value6"),
+                    DropDownValueModel(name: 'name7', value: "value7"),
+                    DropDownValueModel(name: 'name8', value: "value8"),
+                  ],
+                  onChanged: (val) {},
+                ),
               ],
             ),
           ),
