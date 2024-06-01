4550830e6d6bec26c45e5675d2010560c8b3d648
sharathraja <sharathraja@danlawinc.com>
2024-06-01 12:27:38 +0530

    README.md updated

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const TestPage2(),
+      home: const TestPage(),
     );
   }
 }
